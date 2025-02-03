e664456ca92c31b47bc401477cb43b2ba801b9e7
yentlvandamme <yentl.van.damme@gmail.com>
2025-02-03 17:59:18 -0500

    Get boilerplate ready for receiving and parsing commands

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	go build -o ./dist/main
 
 run:
-	go run main.go
+	go run main.go $(ARGS)
 
 cleanMacDev:
 	rm -r ~/Documents/Scribe
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io/fs"
 	"os"
@@ -11,8 +12,49 @@ import (
 var BaseStoragePath string
 var FileStoragePath string
 
+type Command struct {
+	Name        string
+	Description string
+	Execute     func(*Command) error
+
+	// Flags
+	SnippetName    string
+	SnippetContent string
+}
+
+var commands = map[string]*Command{
+	"add": {
+		Name:        "add",
+		Description: "Add a snippet",
+		Execute:     AddSnippet,
+	},
+}
+
 func main() {
 	setup()
+
+	commandName := os.Args[1]
+	command, ok := commands[commandName]
+	if !ok {
+		fmt.Printf("Could not find command: %s", commandName)
+	}
+
+	switch commandName {
+	case "add":
+		flag.StringVar(&command.SnippetName, "name", "", "The snippet you wish to add")
+		flag.StringVar(&command.SnippetContent, "snippet", "", "The snippet you wish to add")
+	}
+	flag.Parse()
+
+	if err := command.Execute(command); err != nil {
+		fmt.Printf("Failed to execute command: %s", commandName)
+	}
+}
+
+func AddSnippet(cmd *Command) error {
+	// Add the snippet
+	fmt.Println("Adding snippet")
+	return nil
 }
 
 func setup() {
